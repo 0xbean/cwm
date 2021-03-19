b8285064e96a1b4094e5df4b08b772023922df87
kimjeff49 <jeffkkim612@gmail.com>
2021-03-18 22:45:10 -0400

    Adding initial skeleton code

---
+++ frontend/src/components/header.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const Header = () => {
+  return (
+    <Head>
+      <meta charSet="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      <meta name="description" content="" />
+      <meta name="theme-color" content="" />
+      <title>Center for World Missions</title>
+    </Head>
+  );
+};
+
+export default Header;
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,14 @@
+import Header from "../components/header";
+
+export default function IndexPage() {
+  return (
+    <>
+      <Header />
+      <div className="test">Hello</div>
+    </>
+  );
+}
+
+// export default function GetServerSideProps({ ctx }) {
+
+// }
