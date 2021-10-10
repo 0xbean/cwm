24ccdbf2913b24b6e1ac29b642ec01c4cf7527af
kimjeff49 <jeffkkim612@gmail.com>
2021-10-10 15:18:08 -0400

    adding a production esq style of deployment

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'cwm-frontend',
+      script: 'npm',
+      args: 'start',
+      cwd: './',
+      instances: 2,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
+  ],
+};
