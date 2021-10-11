a59d6aabfb826166a2f03a890aa9435496f4fea3
kimjeff49 <jeffkkim612@gmail.com>
2021-10-11 17:55:54 -0400

    making edits to ecosystem files

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -10,11 +10,7 @@ module.exports = {
       watch: false,
       max_memory_restart: '1G',
       env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
         NODE_ENV: 'production',
-        PORT: 3000,
       },
     },
   ],
